5a56e494fa09fcaa935c8552f99201bbed08b63d
k-sato <kyokke.matsu@gmail.com>
2025-12-03 14:35:51 +0900

    fix: discordDevに対応させる

--- api/discord.ts
+++ api/discord.ts
@@ -1,10 +1,20 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method !== "POST") {
+  if (req.method === "HEAD" || req.method === "OPTIONS") {
+    res.status(200).send("OK");
+    return;
+  }
+
+  if (req.method === "GET") {
     res.status(200).send("OK (discord minimal endpoint)");
     return;
   }
 
-  res.status(200).json({ type: 1 });
+  if (req.method === "POST") {
+    res.status(200).json({ type: 1 });
+    return;
+  }
+
+  res.status(200).send("OK");
 }
